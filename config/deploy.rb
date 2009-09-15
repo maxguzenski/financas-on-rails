22b94867b949411afa7b069196b597dcce42c86b
Maximiliano Guzenski <max@guzenski.com.br>
2009-09-15 18:53:03 -0300

    rails 2.3.4 freezed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,41 @@
-set :application, "financas-on-rails"
+default_run_options[:pty] = true
+
+set :user, 'guzenski'
+set :domain, 'guzenski.com.br'
+set :application, 'financas-on-rails'
+
+set :deploy_to, "/home/guzenski/for.guzenski.com.br"
+set :deploy_via, :remote_cache
+
 set :repository,  "git://github.com/maxguzenski/financas-on-rails.git"
-
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :scm_verbose, true
+set :use_sudo, false
 
-role :web, "for.guzenski.com.br" 
-role :app, "for.guzenski.com.br"
-role :db,  "mysql.guzenski.com.br", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+server domain, :app, :web
+role :db, domain, :primary => true
+
+#role :web, "for.guzenski.com.br"
+#role :app, "for.guzenski.com.br"
+#role :db,  "mysql.guzenski.com.br", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
+#namespace :deploy do
+#  task :start {}
+#  task :stop {}
+#  task :restart, :roles => :app, :except => { :no_release => true } do
+#    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#  end
+#end
+
 namespace :deploy do
-  task :start {}
-  task :stop {}
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
   end
 end
